8043736ffb5aaa60991ec5b6e0246a657e19fab2
Kwame Opare Asiedu <kwame@opare.dev>
2026-07-08 00:44:27 +0000

    Setup commander CLI application

--- modules/cli/rollup.config.mjs
+++ modules/cli/rollup.config.mjs
@@ -3,10 +3,15 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { typescriptPaths } from "rollup-plugin-typescript-paths";
+import fs from "fs-extra";
+
+const packageInfo = JSON.parse(fs.readFileSync("package.json", { encoding: "utf-8" }));
+const external = Object.keys(packageInfo.dependencies).map((dep) => new RegExp(`${dep}.+`));
 
 export default defineConfig({
   input: "src/index.ts",
   output: { file: "bin/index.js", format: "commonjs" },
   plugins: [nodeResolve(), commonjs(), typescript(), typescriptPaths()],
   treeshake: { moduleSideEffects: false },
+  external: external,
 });
--- modules/cli/src/index.ts
+++ modules/cli/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello World");
+import fs from "fs-extra";
+import { resolve } from "node:path";
+import { Command } from "commander";
+
+const packageInfo = JSON.parse(
+  fs.readFileSync(resolve(__dirname, "../package.json"), { encoding: "utf-8" }),
+);
+
+const cli = new Command("Firedeck");
+cli.description(packageInfo.description);
+cli.version(packageInfo.version, "-v");
+
+cli
+  .command("init <directory>")
+  .description("Create a new Firedeck project at the specified directory")
+  .action((directory) => {
+    const absDirectory = resolve(process.cwd(), directory);
+    console.log({ dir: absDirectory });
+  });
+
+cli.parse(process.argv);
