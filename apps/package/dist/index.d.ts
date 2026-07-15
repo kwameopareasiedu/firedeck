9647cab2e13704977937c1aa9c2ed50488d3faeb
Kwame Opare Asiedu <kwame@opare.dev>
2026-07-15 11:04:05 +0000

    Removed package dist folder from git cache

--- apps/package/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { UserConfig } from 'vite';
-
-interface FiredeckConfig {
-    vite: UserConfig;
-}
-declare function defineConfig(config: FiredeckConfig): FiredeckConfig;
-
-export { defineConfig };
