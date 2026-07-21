559d8bdc0b5b2fa0ec086dff142c5adaa191da35
Kwame Opare Asiedu <kwame@opare.dev>
2026-07-21 01:45:09 +0000

    Completed deploy CLI function

--- apps/cli/src/deploy-project.ts
+++ apps/cli/src/deploy-project.ts
@@ -1,23 +1,17 @@
-import {
-  assertFiredeckRootDir,
-  demoFirebaseProject,
-  error,
-  getProjectPaths,
-  info,
-  warn,
-} from "@/utils";
+import { assertFiredeckRootDir, demoFirebaseProject, getProjectPaths, info } from "@/utils";
 import { analyzeProject, getFiredeckConfig } from "@/analyze-project";
 import { execSync } from "node:child_process";
 import { buildProject } from "@/build-project";
-import fs from "fs-extra";
-import { CompileProjectOptions, NestedArray, NestedRecord } from "@/types";
+import { BackendModuleFunction, CompileProjectOptions, NestedArray, NestedRecord } from "@/types";
+
+const FN_DEPLOY_BATCH_SIZE = 10;
 
 /** Builds and deploys a Firedeck project to Firebase using the global `firebase` command */
 export async function deployProject(rootDir: string, opts?: CompileProjectOptions) {
   assertFiredeckRootDir(rootDir);
 
   const alias = opts?.firebaseProjectAlias;
-  const { runtimeDir, runtimeFirebaseRcFile } = getProjectPaths(rootDir);
+  const { runtimeDir } = getProjectPaths(rootDir);
   const firedeckConfig = await getFiredeckConfig(rootDir);
   const firebaseProjectConfigs = firedeckConfig.firebase?.projects ?? [];
 
@@ -34,18 +28,18 @@ export async function deployProject(rootDir: string, opts?: CompileProjectOption
   })();
 
   const firebaseAuthUser = (() => {
-    const res = queryFirebase<NestedRecord>("login:list", localProjectConfig.projectId);
+    const res = runFirebaseCmd<NestedRecord>("login:list", localProjectConfig.projectId);
     if (!res?.[0]) throw 'run "firebase login" to sign into firebase first';
     return res[0].user;
   })();
 
-  const remoteProjects = queryFirebase<NestedArray>("projects:list", localProjectConfig.projectId);
-  if (!remoteProjects.find((p) => p.projectId === localProjectConfig.projectId))
+  const remoteProjects = runFirebaseCmd<NestedArray>("projects:list", localProjectConfig.projectId);
+  if (!remoteProjects?.find((p) => p.projectId === localProjectConfig.projectId))
     throw `firebase project not found: ${localProjectConfig.projectId}`;
 
   const projectModel = await analyzeProject(rootDir);
-  const remoteApps = queryFirebase<NestedArray>("apps:list", localProjectConfig.projectId);
-  const remoteHostingSites = queryFirebase<NestedRecord>(
+  const remoteApps = runFirebaseCmd<NestedArray>("apps:list", localProjectConfig.projectId);
+  const remoteHostingSites = runFirebaseCmd<NestedRecord>(
     "hosting:sites:list",
     localProjectConfig.projectId,
   )?.sites as NestedArray | undefined;
@@ -54,7 +48,7 @@ export async function deployProject(rootDir: string, opts?: CompileProjectOption
     const clientLocalApp = localProjectConfig.apps({ moduleName: client.name });
     if (!clientLocalApp) throw `no configured firebase app for client module: ${client.name}`;
 
-    const clientRemoteApp = remoteApps.find(
+    const clientRemoteApp = remoteApps?.find(
       (app) => app.platform === "WEB" && app.appId === clientLocalApp.appId,
     );
     if (!clientRemoteApp) throw `no remote firebase app for client module: ${client.name}`;
@@ -73,52 +67,57 @@ export async function deployProject(rootDir: string, opts?: CompileProjectOption
   info(`firebase user: ${firebaseAuthUser.email}`);
   info(`firebase project: ${localProjectConfig.projectId} (${alias})`);
 
-  // await buildProject(rootDir, opts);
+  await buildProject(rootDir, { firebaseProjectAlias: alias });
 
-  //
-  // const localHostingTargets = JSON.parse(
-  //   fs.readFileSync(runtimeFirebaseRcFile, { encoding: "utf-8" }),
-  // ).targets[localFirebaseProject.projectId].hosting;
-  //
-  // for (const localClientName in localHostingTargets) {
-  //   const localClientHostingTargets: string[] = localHostingTargets[localClientName];
-  //
-  //   for (const localClientHostingTarget of localClientHostingTargets) {
-  //     const localClientHostingTargetExists = firebaseHostingSites.find(
-  //       (site: { name: string }) =>
-  //         site.name ===
-  //         `projects/${localFirebaseProject.projectId}/sites/${localClientHostingTarget}`,
-  //     );
-  //
-  //     if (!localClientHostingTargetExists) {
-  //       warn(`hosting site: missing ${localClientHostingTarget}`);
-  //       info(`hosting site: creating ${localClientHostingTarget}`);
-  //       const createSiteResponse = JSON.parse(
-  //         execSync(
-  //           `firebase hosting:sites:create ${localClientHostingTarget} --project ${localFirebaseProject.projectId} --json`,
-  //           { encoding: "utf-8" },
-  //         ),
-  //       );
-  //
-  //       if (createSiteResponse.status === "error") throw createSiteResponse.error;
-  //       info(`hosting site: created: ${localClientHostingTarget}`);
-  //     } else {
-  //       info(`hosting site: found ${localClientHostingTarget}`);
-  //     }
-  //   }
-  // }
-  //
-  // execSync(`firebase deploy -f --project ${localFirebaseProject.projectId}`, {
-  //   cwd: runtimeDir,
-  //   stdio: "inherit",
-  // });
-}
+  runFirebaseCmd("deploy --except functions --dry-run -f", localProjectConfig.projectId, {
+    cwd: runtimeDir,
+    noJson: true,
+  });
 
-function queryFirebase<T>(command: string, projectId: string) {
-  const response = JSON.parse(
-    execSync(`firebase ${command} --project ${projectId} --json`, { encoding: "utf-8" }),
+  const backendFunctions = projectModel.backends.reduce(
+    (fns, backend) => [...fns, ...backend.functions.map((fn) => ({ ...fn, module: backend.name }))],
+    [] as (BackendModuleFunction & { module: string })[],
   );
 
-  if (response.status === "error") throw response.error;
-  return response.result as T;
+  const functionBatchCount = Math.ceil(backendFunctions.length / FN_DEPLOY_BATCH_SIZE);
+
+  for (let idx = 0; idx < functionBatchCount; idx++) {
+    const startIndex = idx * FN_DEPLOY_BATCH_SIZE;
+    const endIndex = Math.min(startIndex + FN_DEPLOY_BATCH_SIZE, backendFunctions.length);
+
+    const batchFunctions = backendFunctions.slice(startIndex, endIndex);
+    const functionDeployCommands = batchFunctions
+      .map((fn) => `functions:${fn.module}:${fn.name}`)
+      .join(",");
+
+    runFirebaseCmd(
+      `deploy --only ${functionDeployCommands} --dry-run -f`,
+      localProjectConfig.projectId,
+      { cwd: runtimeDir, noJson: true },
+    );
+  }
+
+  info("deployment complete!");
+}
+
+function runFirebaseCmd<T>(
+  command: string,
+  projectId: string,
+  opts?: { cwd?: string; noJson?: boolean },
+) {
+  let fullCommand = `firebase ${command} --project ${projectId}`;
+  if (!opts?.noJson) fullCommand += " --json";
+
+  // info(`executing: ${fullCommand}`);
+
+  if (opts?.noJson) {
+    execSync(fullCommand, { cwd: opts?.cwd, stdio: "inherit" });
+    return;
+  } else {
+    const response = execSync(fullCommand, { cwd: opts?.cwd, encoding: "utf-8" });
+    const parsedResponse = JSON.parse(response);
+
+    if (parsedResponse.status === "error") throw parsedResponse.error;
+    return parsedResponse.result as T;
+  }
 }
